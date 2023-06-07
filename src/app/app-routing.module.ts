2b06729f059a142a2ad9b0703b8f70367af02d23
abhishek <abhishek_mi@yonefu.info>
2023-06-07 15:57:59 +0545

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroComponent } from './Modules/Home/hero/hero.component';
+import { TopPageComponent } from './Modules/Home/top-page/top-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'top', pathMatch: 'full' },
+  { path: 'top', component: TopPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { HeroComponent } from './Modules/Home/hero/hero.component';
+import { TopPageComponent } from './Modules/Home/top-page/top-page.component';
+import { DiscoverComponent } from './Modules/Home/discover/discover.component';
+import { CategoryComponent } from './Modules/Home/category/category.component';
+import { FindJobsComponent } from './Modules/Home/find-jobs/find-jobs.component';
+import { AlertComponent } from './Modules/Home/alert/alert.component';
+import { FeaturedCompanyComponent } from './Modules/Home/featured-company/featured-company.component';
+import { TestimonialsComponent } from './Modules/Home/testimonials/testimonials.component';
+import { TestimonialComponent } from './Modules/Home/testimonial/testimonial.component';
 
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    TopPageComponent,
+    DiscoverComponent,
+    CategoryComponent,
+    FindJobsComponent,
+    AlertComponent,
+    FeaturedCompanyComponent,
+    TestimonialsComponent,
+    TestimonialComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    SlickCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
