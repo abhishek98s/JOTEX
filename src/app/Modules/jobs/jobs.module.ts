7e4f795fde1482bd39f75b16040358dd80d43ced
abhishek98s <abhishekshakya98.com@gmail.com>
2023-06-12 15:57:46 +0545

    filter section

--- src/app/Modules/jobs/jobs.module.ts
+++ src/app/Modules/jobs/jobs.module.ts
@@ -6,11 +6,15 @@ import { JobsRoutingModule } from './jobs-routing.module';
 import { JobsHeroComponent } from './jobs-hero/jobs-hero.component';
 import { JobsTopPageComponent } from './jobs-top-page/jobs-top-page.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { FilterComponent } from './filter/filter.component';
+import { SearchResultComponent } from './search-result/search-result.component';
 
 @NgModule({
   declarations: [
     JobsTopPageComponent,
-    JobsHeroComponent
+    JobsHeroComponent,
+    FilterComponent,
+    SearchResultComponent
   ],
   imports: [
     CommonModule,
