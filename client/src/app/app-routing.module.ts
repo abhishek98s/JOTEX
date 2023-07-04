a274c7ce198ea277f6de94d61ed10d2de2b0e76e
abhishek98s <abhishekshakya98.com@gmail.com>
2023-07-04 16:28:03 +0545

    protected the route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,15 +4,16 @@ import { HeroComponent } from './Modules/Home/hero/hero.component';
 import { TopPageComponent } from './Modules/Home/top-page/top-page.component';
 import { JobsTopPageComponent } from './Modules/jobs/jobs-top-page/jobs-top-page.component';
 import { NotfoundTopPageComponent } from './Modules/not-found/notfound-top-page/notfound-top-page.component';
+import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'top', pathMatch: 'full' },
   { path: 'top', component: TopPageComponent },
-  { path: 'jobs', loadChildren: () => import('./Modules/jobs/jobs-routing.module').then(m => m.JobsRoutingModule) },
+  { path: 'jobs', loadChildren: () => import('./Modules/jobs/jobs-routing.module').then(m => m.JobsRoutingModule), canActivate: [AuthGuard] },
   { path: 'login', loadChildren: () => import('./Modules/login/login-routing.module').then(m => m.LoginRoutingModule) },
   { path: 'register', loadChildren: () => import('./Modules/register/register-routing.module').then(m => m.RegisterRoutingModule) },
   { path: '404', component: NotfoundTopPageComponent },
-  { path: '**',  redirectTo: '404'},
+  { path: '**', redirectTo: '404' },
 ];
 
 @NgModule({
--- client/src/app/services/api-services.service.ts
+++ client/src/app/services/api-services.service.ts
@@ -2,18 +2,23 @@ import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http
 import { Injectable } from '@angular/core';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiServicesService {
+  private isLoggedInValue = false;
   private apiUrl = 'http://localhost:5000/api/v1/auth/login'; // Replace with your API endpoint URL
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
+  isLoggedIn(): boolean {
+    return this.isLoggedInValue;
+  }
   login(data: any) {
-    // const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
 
+    this.isLoggedInValue = true;
     return this.http.post<any>(`${this.apiUrl}`, data)
   }
 
@@ -21,5 +26,10 @@ export class ApiServicesService {
     delete body.confPassword;
     let url = 'http://localhost:5000/api/v1/auth/register';
     return this.http.post(url, body);
+  }
+
+  logout() {
+    this.isLoggedInValue = false;
+    this.router.navigate(['/login']);
   }
 }
